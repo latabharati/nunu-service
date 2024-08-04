35f9107ba1aba777a05aab3d13b11a87663ba205
lata.bharati <me.latabharati@gmail.com>
2024-08-04 18:07:46 +0100

    first commit

---
+++ src/main/java/org/hallabol/nunuservice/NunuServiceApplication.java
@@ -0,0 +1,11 @@
+package org.hallabol.nunuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunuServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NunuServiceApplication.class, args);
+    }
+}
